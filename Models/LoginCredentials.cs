0ef6fcbb9d871306f24a11a705320c303428192e
Tobias Ledin <tobias.ledin@gmail.com>
2024-02-28 13:37:47 +0100

    Reinstated LoginCredentials model.

--- Models/LoginCredentials.cs
+++ Models/LoginCredentials.cs
@@ -1,8 +1,17 @@
-﻿namespace Fribergs_Alpha.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Fribergs_Alpha.Models
 {
     public class LoginCredentials
     {
+        [EmailAddress(ErrorMessage = "Please enter a valid email adress")]
+        [MaxLength(50)]
         public string Email { get; set; } = default!;
+
+        [PasswordPropertyText]
+        [MinLength(6, ErrorMessage = "Password must contain atleast 6 characters")]
+        [MaxLength(50, ErrorMessage = "Password cannot be longer than 50 characters")]
         public string Password { get; set; } = default!;
     }
 }
