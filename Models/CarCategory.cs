a8a81b435534f3ee074d5a26d64c524f1c4baa63
sanna <sanna.nyberg@hotmail.se>
2024-02-29 13:41:49 +0100

    Added [Required] to Category property in CarCategory model

--- Models/CarCategory.cs
+++ Models/CarCategory.cs
@@ -1,8 +1,11 @@
-﻿namespace Fribergs_Alpha.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fribergs_Alpha.Models
 {
     public class CarCategory
     {
         public int CarCategoryId { get; set; }
+        [Required]
         public string Category { get; set; } = "";
     }
 }
