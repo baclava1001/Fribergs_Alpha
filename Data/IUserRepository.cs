7e7f490287709c30e4dfd29ae4bbf1525fb47398
Tobias Ledin <tobias.ledin@gmail.com>
2024-02-29 13:19:15 +0100

    Added interface for UserRepository.

---
+++ Data/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Fribergs_Alpha.Models;
+
+namespace Fribergs_Alpha.Data
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> GetAllUsers();
+        IEnumerable<User> GetAllCustomers();
+        IEnumerable<User> GetAllAdmins();
+        User GetUserById(int id);
+        User GetUserByEmail(string email);
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int id);
+
+    }
+}
