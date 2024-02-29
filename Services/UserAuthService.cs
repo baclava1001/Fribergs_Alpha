9df9bfa553e7a24ba428c0b75478dfbd6304b7c1
Tobias Ledin <tobias.ledin@gmail.com>
2024-02-29 13:35:28 +0100

    Adapted UserAuthService to User model.

--- Services/UserAuthService.cs
+++ Services/UserAuthService.cs
@@ -1,4 +1,5 @@
-﻿using Fribergs_Alpha.Data;
+﻿using Fribergs_Alpha.Components.Pages;
+using Fribergs_Alpha.Data;
 using Fribergs_Alpha.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Security.Claims;
@@ -7,17 +8,15 @@ namespace Fribergs_Alpha.Services
 {
     public class UserAuthService
     {
-        private readonly ICustomer _customerRepo;
-        private readonly IAdmin _adminRepo;
+        private readonly IUserRepository _userRepo;
 
-        public UserAuthService(ICustomer customerRepo, IAdmin adminRepo)
+        public UserAuthService(IUserRepository userRepo)
         {
-            _customerRepo = customerRepo;
-            _adminRepo = adminRepo;
+            _userRepo = userRepo;
         }
 
         // Primary user authentication function. Takes in user login credentials (email, password) and searches for
-        // matching user and password in customer- and adminrepositories.
+        // matching user and password in user- and adminrepositories.
         // If match is made, creates a claims profile/list of claims that is forwarded to Login.razor and tied to
         // the users authentication cookie.
         public AuthResult UserLogin(LoginCredentials credentials)
@@ -25,34 +24,26 @@ namespace Fribergs_Alpha.Services
             var result = new AuthResult();
             var claims = new List<Claim>();
 
-            var customer = _customerRepo.GetCustomerByEmail(credentials.Email);
-            if (customer != null && customer.Password == credentials.Password)
+            var user = _userRepo.GetUserByEmail(credentials.Email);
+            if (user != null && user.Password == credentials.Password)
             {
-                claims.Add(new Claim(type: ClaimTypes.Authentication, "Passed"));
-                claims.Add(new Claim(type: ClaimTypes.Role, "Customer"));
-                claims.Add(new Claim(type: ClaimTypes.Name, $"{customer.FirstName} {customer.LastName}"));
-                claims.Add(new Claim(type: ClaimTypes.UserData, customer.CustomerId.ToString()));
-
                 result.AuthSuccess = true;
-                result.RedirectPath = "/customer";
-            }
-            else if (customer == null)
-            {
-                var admin = _adminRepo.GetAdminByEmail(credentials.Email);
-                if (admin != null && admin.Password == credentials.Password)
+
+                if (user.AdminRole)
                 {
                     claims.Add(new Claim(type: ClaimTypes.Authentication, "Passed"));
                     claims.Add(new Claim(type: ClaimTypes.Role, "Admin"));
-                    claims.Add(new Claim(type: ClaimTypes.Name, $"ADMIN {admin.FirstName} {admin.LastName}"));
-                    claims.Add(new Claim(type: ClaimTypes.UserData, admin.AdminId.ToString()));
-
-                    result.AuthSuccess = true;
+                    claims.Add(new Claim(type: ClaimTypes.Name, $"ADMIN {user.FirstName} {user.LastName}"));
+                    claims.Add(new Claim(type: ClaimTypes.UserData, user.UserId.ToString()));
                     result.RedirectPath = "/admin";
                 }
                 else
                 {
-                    result.AuthSuccess = false;
-                    result.ErrorMessage = "Login credentials incorrect";
+                    claims.Add(new Claim(type: ClaimTypes.Authentication, "Passed"));
+                    claims.Add(new Claim(type: ClaimTypes.Role, "Customer"));
+                    claims.Add(new Claim(type: ClaimTypes.Name, $"{user.FirstName} {user.LastName}"));
+                    claims.Add(new Claim(type: ClaimTypes.UserData, user.UserId.ToString()));
+                    result.RedirectPath = "/customer";
                 }
             }
             else
