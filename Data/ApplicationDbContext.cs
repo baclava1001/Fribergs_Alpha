9d904d276bc9b7ef4bbf30d32d89da036fb2ef18
Samed Salman <samed.salman@gmail.com>
2024-03-01 12:00:44 +0100

    Fixed trouble with dbContext. Excluded/commented out everything having to do with Admin and Customer classes.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Fribergs_Alpha.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fribergs_Alpha.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarCategory> CarCategories { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        //public DbSet<Customer> Customers { get; set; }
+        //public DbSet<Admin> Admins { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public ApplicationDbContext()
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Fribergs_Alpha;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        //}
+    }
+}
