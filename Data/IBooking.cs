d93dc3853a41cae55ef65d5efef705ead9bafcd7
Linus <linusanderstedt@gmail.com>
2024-03-04 14:12:12 +0100

    Added Delete, Details and Index for Bookings. Fixed Sorting on Cars. Added a Booking link to the Admins Page. Changed Booking and Car Repository and IBooking to IQueryable.

--- Data/IBooking.cs
+++ Data/IBooking.cs
@@ -4,7 +4,7 @@ namespace Fribergs_Alpha.Data
 {
     public interface IBooking
     {
-        IEnumerable<Booking> GetAllBookings();
+        IQueryable<Booking> GetAllBookings();
         Booking GetBookingById(int? id);
         void AddBooking(Booking booking);
         void UpdateBooking(Booking booking);
