f360f00d32e5e666dde5a613f283fe4ab2524aa4
Tobias Ledin <tobias.ledin@gmail.com>
2024-03-06 17:23:33 +0100

    Added prop for CarCategory Id.

---
+++ Models/DateCategoryFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace Fribergs_Alpha.Models
+{
+    public class DateCategoryFilter
+    {
+        
+        public DateTime FilterStartDate { get; set; } = DateTime.Now;
+        public DateTime FilterEndDate { get; set; } = DateTime.Now.AddDays(30);
+
+        public int CategoryId { get; set; }
+    }
+}
